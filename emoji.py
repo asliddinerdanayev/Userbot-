146dd3d1bf9413fa48d781dd36a40429260adf80
asliddinerdanayev <112606326+asliddinerdanayev@users.noreply.github.com>
2022-09-04 10:14:48 +0500

    Create emoji.py

---
+++ emoji.py
@@ -0,0 +1,11 @@
+from .. import loader
+from asyncio import sleep
+@loader.tds
+class HeartsMod(loader.Module):
+	strings = {"name": "Emoji"}
+	@loader.owner
+	async def emojicmd(self, message):
+		for _ in range(100):
+			for police in ['😀','😃','😄','😁','😆','😅','😂','🤣','😭','😉','😗','😙','😚','😘','🥰','😍','🤩','🥳',''🙃','🙂','🥲']:
+				await message.edit(police)
+				await sleep(0.3)
